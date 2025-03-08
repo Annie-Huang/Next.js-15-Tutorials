2032bab85d15a8eae4cf7edb152b737169c6bdc2
Annie Huang <annie.hn@gmail.com>
2025-03-08 17:54:20 +1100

    Explain dynamicParams in generateStaticParams

--- rendering-demo/src/app/products/[id]/page.tsx
+++ rendering-demo/src/app/products/[id]/page.tsx
@@ -1,3 +1,7 @@
+// export const dynamicParams = false;
+// It is set to true by default by generateStaticParams, which means you can get http://localhost:3000/products/4 and it will be generated under the .next folder when request.
+// If you set it to false, then the page will return a 404 when you request http://localhost:3000/products/4
+
 export async function generateStaticParams() {
   return [{ id: '1' }, { id: '2' }, { id: '3' }];
 }
