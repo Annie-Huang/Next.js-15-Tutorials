8b97e7405137cd7ba8540702ac291076f26ed3d6
Annie Huang <annie.hn@gmail.com>
2025-03-08 15:05:04 +1100

    Explain Static Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Page = () => {
   console.log('About server component');
-  return <h1>About page</h1>;
+  return <h1>About page {new Date().toLocaleTimeString()}</h1>;
 };
 
 export default Page;
