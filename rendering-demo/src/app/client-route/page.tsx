76b649cc52d161d5b2ee770b191ff00001dc2295
Annie Huang <annie.hn@gmail.com>
2025-03-09 17:48:10 +1100

    Copy react-slick npm playground code into client-route/pages.tsx. css copy into globals.css

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,9 +1,35 @@
 'use client';
 
 import { serverSideFunction } from '@/utils/server-utils';
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function ClientRoutePage() {
-  const result = serverSideFunction();
+  // const result = serverSideFunction();
 
-  return <h1>Client route</h1>;
+  // return <h1>Client route</h1>;
+
+  const settings = {
+    dots: true,
+  };
+  return (
+    <div className='image-slider-container'>
+      <Slider {...settings}>
+        <div>
+          <img src='http://placekitten.com/g/400/200' />
+        </div>
+        <div>
+          <img src='http://placekitten.com/g/400/200' />
+        </div>
+        <div>
+          <img src='http://placekitten.com/g/400/200' />
+        </div>
+        <div>
+          <img src='http://placekitten.com/g/400/200' />
+        </div>
+      </Slider>
+    </div>
+  );
 }
