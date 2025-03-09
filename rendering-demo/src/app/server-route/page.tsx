1d113c0e22d190356a6490f34e5f9804da7f8b68
Annie Huang <annie.hn@gmail.com>
2025-03-09 18:02:43 +1100

    Solution is to use a custom component to wrap the 3rd party lib that use client component only

--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -1,32 +1,43 @@
 import { serverSideFunction } from '@/utils/server-utils';
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+
+// import React from 'react';
+// import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+
+import { ImageSlider } from '@/components/ImageSlider';
 
 export default function ServerRoutePage() {
-  // const result = serverSideFunction();
+  const result = serverSideFunction();
   // return <h1>Server Route {result}</h1>;
 
-  const settings = {
-    dots: true,
-  };
+  // const settings = {
+  //   dots: true,
+  // };
+  // return (
+  //   <div className='image-slider-container'>
+  //     <Slider {...settings}>
+  //       <div>
+  //         <img src='https://picsum.photos/400/200' />
+  //       </div>
+  //       <div>
+  //         <img src='https://picsum.photos/400/200' />
+  //       </div>
+  //       <div>
+  //         <img src='https://picsum.photos/400/200' />
+  //       </div>
+  //       <div>
+  //         <img src='https://picsum.photos/400/200' />
+  //       </div>
+  //     </Slider>
+  //   </div>
+  // );
+
+  // Solution is to use a custom component to wrap the 3rd party lib that use client component only.
   return (
-    <div className='image-slider-container'>
-      <Slider {...settings}>
-        <div>
-          <img src='https://picsum.photos/400/200' />
-        </div>
-        <div>
-          <img src='https://picsum.photos/400/200' />
-        </div>
-        <div>
-          <img src='https://picsum.photos/400/200' />
-        </div>
-        <div>
-          <img src='https://picsum.photos/400/200' />
-        </div>
-      </Slider>
-    </div>
+    <>
+      <h1>Server Route {result}</h1>
+      <ImageSlider />
+    </>
   );
 }
