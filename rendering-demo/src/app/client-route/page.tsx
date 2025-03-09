20d30bcaea7c83ef77695a49f11d296fc389ff22
Annie Huang <annie.hn@gmail.com>
2025-03-09 21:15:46 +1100

    Test useTheme in client-route/page.tsx and it worked.

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,35 +1,43 @@
 'use client';
 
 import { serverSideFunction } from '@/utils/server-utils';
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+
+// import React from 'react';
+// import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+
+import { useTheme } from '@/components/theme-provider';
 
 export default function ClientRoutePage() {
   // const result = serverSideFunction();
-
   // return <h1>Client route</h1>;
+  // const settings = {
+  //   dots: true,
+  // };
+  // return (
+  //   <div className='image-slider-container'>
+  //     <Slider {...settings}>
+  //       <div>
+  //         <img src='https://picsum.photos/400/200' />
+  //       </div>
+  //       <div>
+  //         <img src='https://picsum.photos/400/200' />
+  //       </div>
+  //       <div>
+  //         <img src='https://picsum.photos/400/200' />
+  //       </div>
+  //       <div>
+  //         <img src='https://picsum.photos/400/200' />
+  //       </div>
+  //     </Slider>
+  //   </div>
+  // );
 
-  const settings = {
-    dots: true,
-  };
+  const theme = useTheme();
   return (
-    <div className='image-slider-container'>
-      <Slider {...settings}>
-        <div>
-          <img src='https://picsum.photos/400/200' />
-        </div>
-        <div>
-          <img src='https://picsum.photos/400/200' />
-        </div>
-        <div>
-          <img src='https://picsum.photos/400/200' />
-        </div>
-        <div>
-          <img src='https://picsum.photos/400/200' />
-        </div>
-      </Slider>
+    <div style={{ color: theme.colors.primary }}>
+      <h1>Client route page</h1>
     </div>
   );
 }
