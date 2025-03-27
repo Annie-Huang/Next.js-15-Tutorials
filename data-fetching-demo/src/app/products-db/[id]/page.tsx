5e3a3ee5d1b22b992ceba31ef9c4ee31f3d06058
Annie Huang <annie.hn@gmail.com>
2025-03-27 21:21:22 +1100

    Remove everything we moved to product-edit-form.tsx from app/product-db/[id]/page.tsx

--- data-fetching-demo/src/app/products-db/[id]/page.tsx
+++ data-fetching-demo/src/app/products-db/[id]/page.tsx
@@ -1,9 +1,5 @@
-"use client";
-
-import { Submit } from "@/components/submit";
-import { useActionState } from "react";
-import { createProduct, FormState } from "@/actions/products";
 import { getProduct } from "@/prisma-db";
+import EditProductForm from "./product-edit-form";
 
 export default async function EditProductPage({
   params,
@@ -14,67 +10,5 @@ export default async function EditProductPage({
   const { id } = await params;
   const product = await getProduct(parseInt(id));
 
-  const initialState: FormState = {
-    errors: {},
-  };
-
-  const [state, formAction, isPending] = useActionState(
-    createProduct,
-    initialState,
-  );
-
-  // Copy the form from react-form\page.tsx into here initially
-  return (
-    // <form action={createProduct} className="p-4 space-y-4 max-w-96">
-    <form action={formAction} className="p-4 space-y-4 max-w-96">
-      <div>
-        <label className="text-white">
-          Title
-          <input
-            type="text"
-            className="block w-full p-2 text-black border rounded bg-white"
-            name="title"
-          />
-        </label>
-        {state.errors.title && (
-          <p className="text-red-500">{state.errors.title}</p>
-        )}
-      </div>
-      <div>
-        <label className="text-white">
-          Price
-          <input
-            type="number"
-            className="block w-full p-2 text-black border rounded bg-white"
-            name="price"
-          />
-        </label>
-        {state.errors.price && (
-          <p className="text-red-500">{state.errors.price}</p>
-        )}
-      </div>
-      <div>
-        <label className="text-white">
-          Description
-          <textarea
-            className="block w-full p-2 text-black border rounded bg-white"
-            name="description"
-          />
-        </label>
-        {state.errors.description && (
-          <p className="text-red-500">{state.errors.description}</p>
-        )}
-      </div>
-      {/*<button*/}
-      {/*  type="submit"*/}
-      {/*  className="block w-full p-2 text-white bg-blue-500 rounded disabled:bg-gray-500"*/}
-      {/*  disabled={isPending}*/}
-      {/*>*/}
-      {/*  Add Product*/}
-      {/*</button>*/}
-
-      {/* The reason we separate the component is because useFormStatus can only be used in client component and we don't want to make this whole component a client component. */}
-      <Submit />
-    </form>
-  );
+  return <EditProductForm />;
 }
