2d91e217b32c62eacd9a1e0adfc1c8156ea2e689
Annie Huang <annie.hn@gmail.com>
2025-03-29 16:54:42 +1100

    Add removeProduct into actions/products.ts file

--- data-fetching-demo/src/actions/products.ts
+++ data-fetching-demo/src/actions/products.ts
@@ -71,3 +71,8 @@ export async function editProduct(
   await updateProduct(id, title, parseInt(price), description);
   redirect('/products-db');
 }
+
+export async function removeProduct(id: number) {
+  await deleteProduct(id);
+  revalidatePath('/products-db');
+}
