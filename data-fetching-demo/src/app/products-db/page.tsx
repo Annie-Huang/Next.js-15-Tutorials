ce73c7a19774b6f5ae8f3ff04a967f522cbd0d08
Annie Huang <annie.hn@gmail.com>
2025-03-29 22:26:53 +1100

    Add a 'Create product' button on the app/products-db/page.tsx

--- data-fetching-demo/src/app/products-db/page.tsx
+++ data-fetching-demo/src/app/products-db/page.tsx
@@ -1,5 +1,6 @@
 import { getProducts } from '@/prisma-db';
 import { ProductDetail } from '@/app/products-db/product-detail';
+import Link from 'next/link';
 
 export type Product = {
   id: number;
@@ -19,5 +20,15 @@ export default async function ProductsPrismaDBPage({
   console.log(products);
 
   // Shift the entire code into <ProductDetail> as the useOptimistic required to be a client component
-  return <ProductDetail products={products} />;
+  return (
+    <>
+      <ProductDetail products={products} />
+      <Link
+        href='/products-db-create'
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-4'
+      >
+        Create a new product
+      </Link>
+    </>
+  );
 }
