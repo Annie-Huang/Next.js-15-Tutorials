e10fee79b325ab546f27ca1f7e67e138529e4c36
Annie Huang <annie.hn@gmail.com>
2025-03-29 16:51:07 +1100

    Create the delete button in products-db/page.tsx

--- data-fetching-demo/src/app/products-db/page.tsx
+++ data-fetching-demo/src/app/products-db/page.tsx
@@ -28,6 +28,12 @@ export default async function ProductsPrismaDBPage({
           </h2>
           <p>{product.description}</p>
           <p className='text-lg font-medium'>${product.price}</p>
+          <button
+            type='submit'
+            className='px-4 py-2 mt-4 text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-600'
+          >
+            Delete
+          </button>
         </li>
       ))}
     </ul>
