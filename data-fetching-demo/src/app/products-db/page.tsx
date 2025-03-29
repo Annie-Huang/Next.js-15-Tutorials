7f792c7d38badbbfda30186b40d573ab2d849ce3
Annie Huang <annie.hn@gmail.com>
2025-03-29 17:21:01 +1100

    use optimisticProducts and setOptimisticProducts in the template

--- data-fetching-demo/src/app/products-db/page.tsx
+++ data-fetching-demo/src/app/products-db/page.tsx
@@ -19,6 +19,8 @@ export default async function ProductsPrismaDBPage({
   const products: Product[] = await getProducts(query);
 
   // https://react.dev/reference/react/useOptimistic
+  // useOptimistic is a React Hook that lets you show a different state while an async action is underway.
+  // It accepts some state as an argument and returns a copy of that state that can be different during the duration of an async action such as a network request.
   const [optimisticProducts, setOptimisticProducts] = useOptimistic(
     products,
     (currentProducts, productId) => {
@@ -26,9 +28,16 @@ export default async function ProductsPrismaDBPage({
     },
   );
 
+  // This immediately shows the user their changes while the actual deletion happens in the background
+  const removeProductById = async (productId: number) => {
+    setOptimisticProducts(productId);
+    await removeProduct(productId);
+  };
+
   return (
     <ul className='space-y-4 p-4'>
-      {products.map((product) => (
+      {/*{products.map((product) => (*/}
+      {optimisticProducts.map((product) => (
         <li
           key={product.id}
           className='p-4 bg-white shadow-md rounded-lg text-gray-700'
@@ -38,7 +47,8 @@ export default async function ProductsPrismaDBPage({
           </h2>
           <p>{product.description}</p>
           <p className='text-lg font-medium'>${product.price}</p>
-          <form action={removeProduct.bind(null, product.id)}>
+          {/*<form action={removeProduct.bind(null, product.id)}>*/}
+          <form action={removeProductById.bind(null, product.id)}>
             <button
               type='submit'
               className='px-4 py-2 mt-4 text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-600'
