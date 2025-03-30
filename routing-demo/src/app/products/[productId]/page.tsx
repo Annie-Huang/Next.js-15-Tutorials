3a0f6bf00fabf23973fe14c9471af672c584b432
Annie Huang <annie.hn@gmail.com>
2025-03-30 17:35:06 +1100

    Abstract the type Props in products/[productId]/page.tsx

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,9 +1,14 @@
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+
 // export default function ProductDetails() {
-export default async function ProductDetails({
+/*export default async function ProductDetails({
   params,
 }: {
   params: Promise<{ productId: string }>;
-}) {
+}) {*/
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
   return <h1>Details about product {productId}</h1>;
 }
