b7ded7c70257de23523ddfcb49dd8e675b421f1f
Annie Huang <annie.hn@gmail.com>
2025-03-30 15:24:44 +1100

    How to get pathname in not-found.tsx (since it's not allowed to pass in props)

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,9 +1,20 @@
+'use client';
+import { usePathname } from 'next/navigation';
+
 // http://localhost:3000/products/1/reviews/1001
 // If this page does not exist, nextjs wil bubble up to find the first no-found page until it hit the root
+//
+// Also not-found page cannot pass in parameter, if you need to use params info, make a call to usePathname call. Need to be a client component to use usePathname
 export default function NotFound() {
+  const pathname = usePathname();
+  const productId = pathname.split('/')[2];
+  const reviewId = pathname.split('/')[4];
+
   return (
     <div>
-      <h2>Review Not Found</h2>
+      <h2>
+        Review {reviewId} not found for product {productId}
+      </h2>
     </div>
   );
 }
