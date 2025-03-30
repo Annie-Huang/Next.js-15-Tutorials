f2fb38bf9a65f15db2dc0081f4df687b64c10b3c
Annie Huang <annie.hn@gmail.com>
2025-03-30 17:31:29 +1100

    When metadata exists in multiple places along a route, they merge together, with the page metadata overriding layout metadata for matching properties

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,3 +1,8 @@
+// When metadata exists in multiple places along a route, they merge together, with the page metadata overriding layout metadata for matching properties
+export const metadata = {
+  title: 'About Codevolution',
+};
+
 export default function About() {
   return <h1>About me</h1>;
 }
