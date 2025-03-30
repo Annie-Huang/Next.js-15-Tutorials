ee1942b5b349ab3b2f945c748101cf4e0ca1b805
Annie Huang <annie.hn@gmail.com>
2025-03-30 17:50:01 +1100

    Solution, abstract the client component into a seperated named export component. Keep the metadata into a server component.

--- routing-demo/src/app/counter/page.tsx
+++ routing-demo/src/app/counter/page.tsx
@@ -1,16 +1,19 @@
-'use client';
-import { useState } from 'react';
+// 'use client';
+// import { useState } from 'react';
+import { Counter } from '@/app/counter/counter';
 
 export const metadata = {
   title: 'Counter',
 };
 
 export default function CounterPage() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
-  );
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <p>Count: {count}</p>
+  //     <button onClick={() => setCount(count + 1)}>Increment</button>
+  //   </div>
+  // );
+
+  return <Counter />;
 }
