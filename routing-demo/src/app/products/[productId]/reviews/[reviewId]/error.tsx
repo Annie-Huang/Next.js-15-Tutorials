e07ad56422f853aac2461861428b3612fd2a457b
Annie Huang <annie.hn@gmail.com>
2025-04-07 20:22:18 +1000

    Use reset() to retry rending the page.

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,19 @@
 'use client';
 // Error boundary have to be client component!!
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <div style={{ color: 'red' }}>{error.message}</div>;
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div style={{ color: 'red' }}>
+      {error.message}
+      {/* let user to retry to render the page.
+       */}
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
 }
