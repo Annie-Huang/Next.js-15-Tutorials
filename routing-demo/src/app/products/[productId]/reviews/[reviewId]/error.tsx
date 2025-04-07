a25c45c65dbe8a4d076d6012916d36c7b808ddef
Annie Huang <annie.hn@gmail.com>
2025-04-07 20:28:51 +1000

    Explain how to do reset() on server side component

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,9 @@
 'use client';
 // Error boundary have to be client component!!
 
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
+
 export default function ErrorBoundary({
   error,
   reset,
@@ -8,12 +11,26 @@ export default function ErrorBoundary({
   error: Error;
   reset: () => void;
 }) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
     <div style={{ color: 'red' }}>
       {error.message}
-      {/* let user to retry to render the page.
-       */}
-      <button onClick={() => reset()}>Try again</button>
+      {/* let user to retry to render the page. But this have to be a client side page.
+          server side recovery has to use start transition (from react) + use router (from next)
+      */}
+      {/*<button onClick={() => reset()}>Try again</button>*/}
+
+      {/* 1. First keep refreshing http://localhost:3000/products/1/reviews/1 until you see the error.tsx page.
+          2. Then keep clicking the 'Try again' button until you see you recover from rendering on http://localhost:3000/products/1/reviews/1
+      */}
+      <button onClick={reload}>Try again</button>
     </div>
   );
 }
