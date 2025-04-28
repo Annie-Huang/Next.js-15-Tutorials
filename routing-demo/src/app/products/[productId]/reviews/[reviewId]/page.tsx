c1335987d73289d778ed7941d58bd250cd24a76d
Annie Huang <annie.hn@gmail.com>
2025-04-28 16:42:02 +1000

    Comment out the error in /products/[productId]/reviews/[reviewId]/page.tsx to avoid the confusing

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -9,14 +9,14 @@ export default async function ProductReview({
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
-  // will return either 0 or 1
+  /*  // will return either 0 or 1
   const random = getRandomInt(2);
   // Will load the error.tsx when we get into error, you can keep refreshing the page a few times and you will see it.
   // But keep in mind that the rest of the component, like the layout and the [productId]/page.tsx still shows.
   // Only the [reviewId]\page.tax section has been replaced by the error.tsx
   if (random === 1) {
     throw new Error('Error loading review');
-  }
+  }*/
 
   const { productId, reviewId } = await params;
 
