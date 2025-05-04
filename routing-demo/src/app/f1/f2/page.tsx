c383d214feade9bd14a73bd649de5eabdb48123a
Annie Huang <annie.hn@gmail.com>
2025-05-04 16:03:06 +1000

    Add the link of F4 into F2 page.

--- routing-demo/src/app/f1/f2/page.tsx
+++ routing-demo/src/app/f1/f2/page.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function F2() {
-  return <h1>F2 page</h1>;
+  return (
+    <>
+      <h1>F2 page</h1>
+      <div>
+        <Link href='/f4'>F4</Link>
+      </div>
+    </>
+  );
 }
